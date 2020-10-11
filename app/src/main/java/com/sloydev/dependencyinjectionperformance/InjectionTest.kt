cc9f2d23b092d6d934c101d399605fc799c30f78
lyrafelipe <appslyraf@gmail.com>
2020-10-11 16:18:32 -0300

    remove dagger

--- app/src/main/java/com/sloydev/dependencyinjectionperformance/InjectionTest.kt
+++ app/src/main/java/com/sloydev/dependencyinjectionperformance/InjectionTest.kt
@@ -3,19 +3,14 @@ package com.sloydev.dependencyinjectionperformance
 import android.content.Context
 import org.koin.core.KoinComponent
 import org.koin.core.get
-import javax.inject.Inject
 
 class InjectionTest : KoinComponent {
-
-    private val kotlinDaggerTest = KotlinDaggerTest()
-    private val javaDaggerTest = JavaDaggerTest()
 
     private val rounds = 100
 
     fun runTests(context: Context): List<LibraryResult> {
         return listOf(
             koinTest(),
-            daggerTest(context)
         )
     }
 
@@ -38,27 +33,5 @@ class InjectionTest : KoinComponent {
                 )
             )
         )
-    }
-
-    private fun daggerTest(context: Context): LibraryResult {
-        log("Running Dagger...")
-        return LibraryResult("Dagger", mapOf(
-            Variant.KOTLIN to runTest(
-                test = { (context as App).kotlinComponent.inject(kotlinDaggerTest) }
-            ),
-            Variant.JAVA to runTest(
-                test = { (context as App).javaComponent.inject(javaDaggerTest) }
-            )
-        ))
-    }
-
-    class KotlinDaggerTest {
-        @Inject
-        lateinit var daggerFib8: Fib300
-    }
-
-    class JavaDaggerTest {
-        @Inject
-        lateinit var daggerFib8: FibonacciJava.Fib300
     }
 }
